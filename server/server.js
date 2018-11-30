74bf29f67ac33463f69d2610a86454cbbfc65306
csergey8 <c.sergey@protonmail.com>
2018-11-30 23:18:01 +0200

    products get routes with sorting

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.DATABASE);
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // Middleware 
 const { auth } = require('./middleware/auth');
@@ -41,6 +42,57 @@ app.get('/api/product/woods', (req, res) => {
   Wood.find({}, (err, woods) => {
     if(err) return res.status(400).send(err);
     res.status(200).json(woods)
+  })
+})
+
+//
+// PRODUCTS
+//
+
+app.get('/api/product/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : 'id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+  let skip = req.query.skip ? req.query.skip : null;
+  
+  Product
+  .find()
+  .populate('brand')
+  .populate('wood')
+  .sort([[sortBy, order]])
+  .limit(limit)
+  .exec((err, articles) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(articles);
+  })
+})
+
+app.get('/api/product/articles_by_id', (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if(type =  "array") {
+    let ids = req.query.id.split(',');
+    items = [];
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item)
+    })
+  }
+
+  Product.find({'_id': {$in: items}})
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs) => {
+      return res.status(200).send(docs)
+    })
+})
+
+app.post('/api/product/article', auth, admin, (req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    res.status(200).json({success: true, article: doc})
   })
 })
 
