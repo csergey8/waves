445d0fe9e86583bc70fd9943e5d4bdb65c98133b
csergey8 <c.sergey@protonmail.com>
2018-12-10 10:15:22 +0200

    cards section finished

---
+++ client/src/actions/product_actions.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+import { GET_PRODUCTS_BY_SELL, GET_PRODUCTS_BY_ARRIVAL } from './types';
+
+import { PRODUCT_SERVER } from '../utils/misc';
+
+export function getProductsBySell() {
+  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
+    .then(res => res.data);
+
+    return {
+      type: GET_PRODUCTS_BY_SELL,
+      payload: request
+    }
+}
+
+
+export function getProductsByArrival() {
+  const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=createdAt&order=desc&limit=4`)
+    .then(res => res.data);
+
+    return {
+      type: GET_PRODUCTS_BY_ARRIVAL,
+      payload: request
+    }
+}
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotions from './home_promotions';
+import CardBlock from '../../utils/card_block';
+import { getProductsBySell, getProductsByArrival } from '../../actions/product_actions';
+import { connect } from 'react-redux';
 
-export default class Home extends Component {
+
+class Home extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(getProductsBySell());
+    this.props.dispatch(getProductsByArrival());
+
+  }
   render() {
     return (
       <div>
         <HomeSlider />
+        <CardBlock
+         list={this.props.products.bySell}
+         title="Best Selling guitars"
+        />
         <HomePromotions />
+        <CardBlock
+         list={this.props.products.byArrival}
+         title="New arrivals"
+        />
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    products: state.products
+  }
+}
+
+export default connect(mapStateToProps)(Home);
