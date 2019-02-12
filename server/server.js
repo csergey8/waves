adfe80991160e81e45bb6f926761eca2e04697bd
csergey8 <c.sergey@protonmail.com>
2019-02-12 15:04:39 +0200

    change port env variable name

--- server/server.js
+++ server/server.js
@@ -422,7 +422,7 @@ if(process.env.NODE_ENV === "production") {
 
 
 
-const port = process.env.port || 3002;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
